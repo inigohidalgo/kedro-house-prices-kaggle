87ce80a43768d67924742114a60c6622ee788d55
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2022-04-24 20:45:34 +0200

    feat: registered data_processing pipeline

--- src/kaggle_house_prices/pipeline_registry.py
+++ src/kaggle_house_prices/pipeline_registry.py
@@ -3,6 +3,8 @@ from typing import Dict
 
 from kedro.pipeline import Pipeline, pipeline
 
+from kaggle_house_prices.pipelines import data_processing as dp
+
 
 def register_pipelines() -> Dict[str, Pipeline]:
     """Register the project's pipelines.
@@ -10,4 +12,9 @@ def register_pipelines() -> Dict[str, Pipeline]:
     Returns:
         A mapping from a pipeline name to a ``Pipeline`` object.
     """
-    return {"__default__": pipeline([])}
+    raw_to_intermediate_pipeline = dp.create_pipeline()
+
+    return {
+        "__default__": pipeline([]),
+        "raw_to_intermediate": raw_to_intermediate_pipeline,
+    }
--- src/kaggle_house_prices/pipelines/data_processing/pipeline.py
+++ src/kaggle_house_prices/pipelines/data_processing/pipeline.py
@@ -4,7 +4,16 @@ generated using Kedro 0.18.0
 """
 
 from kedro.pipeline import Pipeline, node, pipeline
+from .nodes import raw_to_intermediate_houses
 
 
 def create_pipeline(**kwargs) -> Pipeline:
-    return pipeline([])
+    return pipeline(
+        [
+            node(
+                func=raw_to_intermediate_houses,
+                inputs="house_prices",
+                outputs="intermediate_house_prices",
+            )
+        ]
+    )
