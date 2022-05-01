4dfb2ebbd854223c4b32e53494794d22ce553df8
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2022-05-01 02:33:08 +0200

    feat: added extract_target fn to utils

--- src/kaggle_house_prices/utils/data_processing_utils.py
+++ src/kaggle_house_prices/utils/data_processing_utils.py
@@ -1,8 +1,10 @@
+from typing import Tuple, Union
+
 import pandas as pd
 import numpy as np
 import logging
 
-log = logging.getLogger()
+log = logging.getLogger(__name__)
 
 def _is_true(series: pd.Series, true_key="Y", false_key=None) -> pd.Series:
     log.info("Wasupp")
@@ -16,3 +18,16 @@ def _is_true(series: pd.Series, true_key="Y", false_key=None) -> pd.Series:
     true_series = pd.Series(true_values, index=series.index, name=series.name).astype(bool)
 
     return true_series
+
+def extract_target_from_df(df, target_name=None, return_Xy=True) -> Union[Tuple[pd.DataFrame, pd.Series], pd.Series]:
+    if target_name is None:
+        target_name = "target"
+        log.info(f"No target name specified, assuming default: {target_name}")
+    log.info(f"Extracting target {target_name} from df")
+    target_series = df[target_name]
+    if return_Xy:
+        log.debug("Returning df and target as (X, y)")
+        return df.drop(columns=target_name), target_series
+    else:
+        log.debug("Returning target variable as Series")
+        return df[target_name]
