afccb54401158bb48ae052a82b1d872af9e1d750
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2022-05-03 19:27:31 +0200

    feat: added performance utils scoring function

--- src/kaggle_house_prices/utils/performance.py
+++ src/kaggle_house_prices/utils/performance.py
@@ -1,7 +1,10 @@
 from sklearn import metrics
+import logging
 
+log = logging.getLogger(__name__)
 
 def get_scoring_function(function_name):
+    log.debug(f"Getting scoring function {function_name}")
     metrics_dict = {
         "r2": metrics.r2_score,
         "mse": metrics.mean_squared_error,
