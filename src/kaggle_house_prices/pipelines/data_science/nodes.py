38655b8348dfc26c77afb0f6f7b6af3a37c04748
Iñigo Hidalgo Rey <inigohrey@gmail.com>
2022-06-22 20:44:04 +0200

    feat: added ClassHolder

--- src/kaggle_house_prices/pipelines/data_science/nodes.py
+++ src/kaggle_house_prices/pipelines/data_science/nodes.py
@@ -3,7 +3,7 @@ This is a boilerplate pipeline 'data_science'
 generated using Kedro 0.18.0
 """
 import logging
-
+import importlib
 from sklearn import model_selection
 
 from mlflow import (
@@ -21,6 +21,24 @@ set_tracking_uri("http://127.0.0.1:12347/")
 
 
 log = logging.getLogger(__name__)
+
+
+def import_string(dotted_path):
+    """
+    Import a dotted module path and return the attribute/class designated by the
+    last name in the path. Raise ImportError if the import failed.
+
+    From: https://stackoverflow.com/a/34963527/9807171
+    """
+    try:
+        module_path, class_name = dotted_path.rsplit(".", 1)
+    except ValueError as err:
+        raise ImportError("%s doesn't look like a module path" % dotted_path) from err
+
+    try:
+        return getattr(module_path, class_name)
+    except AttributeError as err:
+        raise ImportError('Module "%s" does not define a "%s" attribute/class' % (module_path, class_name)) from err
 
 
 def train_test_split(input_df, train_test_split_options):
@@ -51,17 +69,34 @@ def train_model_on_df(
     return model_object
 
 
-def get_model_class(model_name):
-    """
-    Returns the model class
-    """
-    if model_name == "rforest":
-        from sklearn.ensemble import RandomForestRegressor
+class ClassHolder:
+    def __init__(self, accept_module_str=True):
+        self.classes = {}
+        self.flexible_import = accept_module_str
 
-        return RandomForestRegressor
-    elif model_name == "elastic":
-        from sklearn.linear_model import ElasticNet
+    def add_class(self, c, class_name=None):
+        key = class_name if class_name else c.__name__
+        self.classes[key] = c
 
-        return ElasticNet
-    else:
-        raise ValueError(f"Unknown model name: {model_name}")
+    def held(self, c):
+        self.add_class(c)
+
+        return c
+
+    def __getitem__(self, key):
+
+        try:
+            return self.classes[key]
+        except KeyError as e:
+            if self.flexible_import:
+                return import_string(key)
+            else:
+                raise e
+
+
+model_holder = ClassHolder()
+from sklearn.ensemble import RandomForestRegressor
+from sklearn.linear_model import ElasticNet
+
+model_holder.add_class(RandomForestRegressor)
+model_holder.add_class(ElasticNet)
